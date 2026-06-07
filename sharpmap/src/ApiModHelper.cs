4909b72389244d2822004cdb21f7bdac0f706c3f
Psyloh <psylozoff@gmail.com>
2026-06-07 16:24:55 +0200

    Add project files.

---
+++ sharpmap/src/ApiModHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace SharpMap
+{
+	public static class ApiModHelper
+	{
+		static ICoreServerAPI? _sApi;
+		public static ICoreServerAPI SApi
+		{
+			get => _sApi ?? throw new Exception("SApi is null");
+			set => _sApi = value;
+		}
+
+		static ICoreClientAPI? _cApi;
+		public static ICoreClientAPI CApi
+		{
+			get => _cApi ?? throw new Exception("CApi is null");
+			set => _cApi = value;
+		}
+
+		static Mod? _mod;
+		public static Mod Mod
+		{
+			get => _mod ?? throw new Exception("Mod is null");
+			set => _mod = value;
+		}
+
+		public static void Error(string message) => Mod.Logger.Error(message);
+		public static void Error(Exception ex) => Mod.Logger.Error(ex);
+		public static void Warning(string message) => Mod.Logger.Warning(message);
+		public static void Notification(string message) => Mod.Logger.Notification(message);
+
+		public static string ModId => Mod.Info.ModID;
+
+		public static IClientNetworkChannel GetClientChannel() => CApi.Network.GetChannel(ModId);
+		public static IServerNetworkChannel GetServerChannel() => SApi.Network.GetChannel(ModId);
+
+
+	}
+}
---
+++ sharpmap/src/sharpmapModSystem.cs
@@ -0,0 +1,117 @@
+﻿using ProtoBuf;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace SharpMap
+{
+	[ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+	public class Init
+	{
+
+	}
+
+	[ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+	public class LayerData
+	{
+
+	}
+
+	[ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+	public class LayerUpdate
+	{
+		public int LayerId {  get; set; }
+		public int RegionId { get; set; }
+	}
+
+	public class ServerProvider
+	{
+		public void Process(IServerPlayer player, LayerUpdate update)
+		{
+
+		}
+	}
+
+	public interface IClientProvider
+	{
+		void Process(LayerData data);
+	}
+
+	public class HeadlessClientProvider : IClientProvider
+	{
+		public void Process(LayerData data)
+		{
+
+		}
+	}
+
+	public class ClientProvider : IClientProvider
+	{
+		public void Process(LayerData data)
+		{
+
+		}
+	}
+
+	public class LayerManager
+	{
+
+	}
+
+	public class SharpMapModSystem : ModSystem
+	{
+		ServerProvider? _serverProvider;
+		IClientProvider? _clientProvider;
+
+		public override void Start(ICoreAPI api)
+		{
+			ApiModHelper.Mod = Mod;
+
+			if (api is ICoreClientAPI capi)
+			{
+				ApiModHelper.CApi = capi;
+			}
+			else if (api is ICoreServerAPI sapi)
+			{
+				ApiModHelper.SApi = sapi;
+			}
+			api.Network.RegisterChannel(Mod.Info.ModID)
+				.RegisterMessageType<Init>()
+				.RegisterMessageType<LayerUpdate>();
+		}
+
+		public override void StartServerSide(ICoreServerAPI api)
+		{
+			_serverProvider = new ServerProvider();
+
+			ApiModHelper.GetServerChannel()
+				.SetMessageHandler<LayerUpdate>(_serverProvider.Process);
+
+			api.Event.PlayerJoin += PlayerJoin;
+		}
+
+		void PlayerJoin(IServerPlayer player)
+		{
+			ApiModHelper.GetServerChannel().SendPacket(new Init(), player);
+		}
+
+		public override void StartClientSide(ICoreClientAPI api)
+		{
+			ApiModHelper.GetClientChannel()
+				.SetMessageHandler<Init>(data =>
+				{
+					_clientProvider = new ClientProvider();
+				})
+				.SetMessageHandler<LayerData>(_clientProvider!.Process);
+		}
+
+		public override void AssetsFinalize(ICoreAPI api)
+		{
+			if (api.Side == EnumAppSide.Server || ApiModHelper.GetClientChannel() != null)
+			{
+				return;
+			}
+			_clientProvider = new HeadlessClientProvider();
+		}
+	}
+}
